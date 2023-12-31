2f377c707cc1619068c3a1e9bdf133ed9fee11c6
Malak Muhammad <126984039+malakmuhammaddd@users.noreply.github.com>
2023-12-31 23:10:46 +0200

    algo.py

--- Maxsubarray Code In Python/algo.py
+++ Maxsubarray Code In Python/algo.py
@@ -1,11 +1,11 @@
-def max_subarray_condition(A, L, R):
+def MaxSubarray(A, L, R):
     if L > R:
         return float('-inf')
     if L == R:
         return A[L]
     mid = (L + R) // 2
-    max1 = max_subarray_condition(A, L, mid)
-    max2 = max_subarray_condition(A, mid + 1, R)
+    max1 = MaxSubarray(A, L, mid)
+    max2 = MaxSubarray(A, mid + 1, R)
     sum1, sum2 = 0, 0
     max3, max4 = float('-inf'), float('-inf')
     for j in range(mid, L , -1):
@@ -21,5 +21,5 @@ def max_subarray_condition(A, L, R):
 
 # Example:
 A = [3,-5,2,-1,5,-6,1,-2]
-result = max_subarray_condition(A, 0, len(A)-1)
+result = MaxSubarray(A, 0, len(A)-1)
 print(result)
